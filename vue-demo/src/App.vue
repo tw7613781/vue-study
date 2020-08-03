ce143455babd1a2cecf9866e61158ee4fc6b9084
Max Tang <tw7613781@gmail.com>
2020-08-04 02:59:06 +0900

    feat: 更新一些vuejs的笔记

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    {{message}} {{message + message}}
+    <div :id="message"></div>
+    <!-- <todo-item v-for="item in list" :title="item.title" :del="item.del"></todo-item> -->
+    <TodoList>
+      <TodoItem @delete="handleDelete" v-for="item in list" :title="item.title" :key="item.title" :del="item.del">
+        <!-- <span slot="pre-icon">前置图标</span>
+        <span slot="suf-icon">后置图标</span> -->
+        <template v-slot:pre-icon="{value}">
+          <span class="red">前置图标 {{value}}</span>
+        </template>
+        <template v-slot:suf-icon>
+          <span>后置图标</span>
+        </template>
+      </TodoItem>
+    </TodoList>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    TodoItem
+  },
+  data() {
+    return {
+      message: 'hello world',
+      list: [{
+              title: '课程1',
+              del: false
+            }, {
+              title: '课程2',
+              del: true
+          }]
+      }
+  },
+  methods: {
+    handleDelete(val) {
+      console.log("handleDelete", val)
+    }
+  }
+}
+</script>
+<style scoped>
+.red {
+  color: red;
+}
+</style>
---
+++ vue-demo/src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <slot name="pre-icon" :value="val"></slot>
+    <span v-if="!del">{{title}}</span>
+    <span v-else style="text-decoration: line-through;">{{title}}</span>
+    <slot name="suf-icon"></slot>
+    <button v-show="del" @click="handleClick">删除</button>
+  </li>
+</template>
+<script>
+export default {
+  props: {
+        title: String,
+        del: {
+          type: Boolean,
+          default: false
+        }
+      },
+  data: function () {
+          return {
+            val: Math.random()
+          }
+        },
+  methods: {
+    handleClick() {
+      console.log('点击删除按钮')
+      this.$emit('delete', this.title)
+    }
+  }
+}
+</script>
